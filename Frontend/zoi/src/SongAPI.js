8c9c2acd2544abe9e593c75d99c902f25481f9c5
manasghandat <95558940+manasghandat@users.noreply.github.com>
2022-10-28 08:16:57 +0530

    Add files via upload

---
+++ Frontend/zoi/src/SongAPI.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const bodyparser = require('body-parser');
+
+var Songs = [];
+
+const options = {
+  method: 'GET',
+  url: 'https://spotify23.p.rapidapi.com/playlist_tracks/',
+  params: {id: '37i9dQZF1DX0XUsuxWHRQd', offset: '0', limit: '100'},
+  headers: {
+    'X-RapidAPI-Key': '6256c0dd26msh79a991a8ed2bd6cp111c67jsne39508529361',
+    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	const Spotify = response.data;
+    let size = Math.min(Spotify.limit,Spotify.total);
+    for (let index = 0; index < size; index++) {
+        let song = {
+            "Song preview" : Spotify.items[index].track.preview_url,
+            "Song Link" : Spotify.items[index].track.external_urls.spotify,
+            "Song Name" : Spotify.items[index].track.name,
+            "Artist Name":Spotify.items[index].track.artists[0].name,
+            "Album Name":Spotify.items[index].track.album.name,
+            "Duration" : Spotify.items[index].track.duration_ms,
+            "Song Image" : Spotify.items[index].track.album.images[0].url
+        };
+        Songs.push(JSON.stringify(song));
+    }
+    // console.log(Songs);
+}).catch(function (error) {
+	console.error(error);
+});
